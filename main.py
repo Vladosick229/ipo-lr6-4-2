76f6d86314d3c4287601c87becd4b3759bd99072
Vlad Sevruk <vladworking5@gmail.com>
2024-10-18 16:18:10 +0300

    №2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-spis=[-15, -4, -2, -7, 0, 3, 5, 12, 9]
-
-random_spis = random.choice(spis)
+import random
+numbers = [-15, -4, -2, -7, 0, 3, 5, 12, 9]
+random_number = random.choice(numbers)
+print(random_number)
